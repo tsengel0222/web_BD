2e7eea356e9c1ab9aaed02cdb75a86bf4829a90f
Battsengel <batcengelbatzargal64@gmail.com>
2024-04-24 17:23:20 +0800

    new chamges

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,12 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './App.css'
-
+import Navbar from './Components/Navbar/Navbar'
+import Admin from './Pages/Admin/Admin'
 function App() {
   return (
     <div>
-
+      <Navbar/>
+      <Admin/>
     </div>
 
   )
